682a29c398061c8a9a5865a7651243a3cdf8bc4b
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2020-11-17 23:56:24 +0100

    update init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Draw me a sheep`,
-    description: `Graphic Designer.`,
+    title: `Conseil piscine`,
+    description: `Conseil Piscine guide.`,
     author: `@drawmeasheep`,
   },
   plugins: [
+    'gatsby-plugin-preact',
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
@@ -25,7 +26,7 @@ module.exports = {
         background_color: `#303030`,
         theme_color: `#303030`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
